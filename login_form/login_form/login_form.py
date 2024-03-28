776612e1410966bd4d4f1537ae4b0508d7510072
Daniel Morala <morala84@gmail.com>
2024-03-28 18:29:01 +0100

    test api

--- login_form/login_form/login_form.py
+++ login_form/login_form/login_form.py
@@ -1,6 +1,8 @@
-from login_form.pages import *  # noqa: F403
-
 import reflex as rx
+from login_form.pages import *  # noqa: F403
+from login_form.api.api import hello
 
 
 app = rx.App()
+
+app.api.add_api_route("/hello", hello)
